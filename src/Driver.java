a4ac03e5ddcfbad7f05c0540adb18dd51473c097
Sophie Engle <sjengle@cs.usfca.edu>
2016-09-01 14:42:15 -0700

    Creating project template.

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 
 public class Driver {
 
     public static void main(String[] args) {
         // TODO Modify as necessary!
-        System.out.println(args);
+        System.out.println(Arrays.toString(args));
     }
 }
