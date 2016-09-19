8b6fadddcde1028831ab673cf43ae144a69ddbcd
Nick The Sick <nperez0111@gmail.com>
2016-09-18 20:46:01 -0700

    Added traversal of files

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Driver {
 
 	public static void main( String[] args ) {
 		// TODO Modify as necessary!
 		ArgumentParser parser = new ArgumentParser( args );
-
+		Path dir = Paths.get( parser.getValue( "-dir" ) ).toAbsolutePath().normalize();
+		System.out.println( dir.toString() );
+		try {
+			System.out.print( Traverser.validFiles( dir ) );
+		}
+		catch ( IOException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/Traverser.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+public class Traverser {
+	private Path path;
+
+	public Traverser() {
+
+	}
+
+	public Traverser( Path p ) {
+		path = p;
+	}
+
+	private static boolean isOfCorrectType( Path path ) {
+		return path.getFileName().toString().toLowerCase().endsWith( ".txt" );
+	}
+
+	private static ArrayList<Path> validFiles( Path path, ArrayList<Path> accumulator ) throws IOException {
+
+		try ( DirectoryStream<Path> listing = Files.newDirectoryStream( path ) ) {
+
+			for ( Path file : listing ) {
+
+				if ( Files.isDirectory( file ) ) {
+
+					validFiles( file, accumulator );
+
+				}
+
+				else {
+
+					if ( isOfCorrectType( file ) ) {
+
+						accumulator.add( file );
+
+					}
+
+				}
+
+			}
+
+		}
+
+		return accumulator;
+	}
+
+	public static ArrayList<Path> validFiles( Path path ) throws IOException {
+		return validFiles( path, new ArrayList<Path>() );
+	}
+
+	public boolean perform( File f ) {
+		return false;
+	}
+}
