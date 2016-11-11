e7cf70aa76b3da0948fad039145679b7d7fc2953
Nick The Sick <nperez0111@gmail.com>
2016-11-10 16:53:58 -0800

    Some more URL Queue work

--- src/URLQueue.java
+++ src/URLQueue.java
@@ -9,12 +9,19 @@ public class URLQueue {
 
 	public static final int SIZE = 50;
 
+	/**
+	 * adds a url queue to the queue
+	 * 
+	 * @param url
+	 * @return false if queue is full true if the url is in the queue or added
+	 *         to the queue
+	 */
 	public static boolean addToQueue( URL url ) {
 
 		if ( urls.size() < SIZE ) {
 			for ( URL u : urls ) {
 				if ( u.sameFile( url ) ) {
-					return false;
+					return true;
 				}
 			}
 			urls.add( url );
@@ -23,6 +30,11 @@ public class URLQueue {
 		return false;
 	}
 
+	/**
+	 * returns the first element in the queue
+	 * 
+	 * @return url to parse
+	 */
 	public static URL popQueue() {
 
 		if ( count + 1 < SIZE ) {
@@ -32,6 +44,9 @@ public class URLQueue {
 		return null;
 	}
 
+	/**
+	 * clears out the urlqueue to reset it
+	 */
 	public static void clear() {
 
 		urls.clear();
