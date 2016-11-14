ebd4406fa9e93498155acf7f4231b41c6770fb63
Nick The Sick <nperez0111@gmail.com>
2016-11-14 10:44:16 -0800

    driver stuff

--- src/Driver.java
+++ src/Driver.java
@@ -18,71 +18,82 @@ public class Driver {
 	private final static String EXACT = "-exact";
 
 	/**
-	 * TODO
+	 * First method to be called parses arguments and calls the correct methods
+	 * with the arguments passed in
+	 * 
 	 * @param args
 	 */
 	public static void main( String[] args ) {
 
 		ArgumentParser parser = new ArgumentParser( args );
 		InvertedIndex index = new InvertedIndex();
-		
-		try {
-			if ( parser.hasFlag( DIR ) && parser.hasValue( DIR ) ) {
-				Path inputIndex = getValidDirectory( parser.getValue( DIR ) );
-				if ( inputIndex != null ) {
+
+		if ( parser.hasValue( DIR ) ) {
+			Path inputIndex = getValidDirectory( parser.getValue( DIR ) );
+			if ( inputIndex != null ) {
+				try {
 					InvertedIndexBuilder.build( inputIndex, index );
 				}
-				
-				// TODO Move writing the index into a separate if block
-				Path outputIndex = getOutputFile( parser, INDEX, "index.json" );
+				catch ( IOException e ) {
+					System.out.println( "Directory " + inputIndex.toString() + ", Not Found or Non-Existant" );
+				}
+			}
+		}
+		if ( parser.hasFlag( INDEX ) ) {
+			Path outputIndex = getOutputFile( parser, INDEX, "index.json" );
 
-				if ( outputIndex != null ) {
+			if ( outputIndex != null ) {
 
+				try {
 					index.toJSON( outputIndex );
-
 				}
+				catch ( IOException e ) {
+					System.out.println( "Outputting index to: " + outputIndex.toString() + " failed" );
+				}
+
 			}
+		}
+		if ( parser.hasValue( EXACT ) ) {
+			Path queryFile = getValidDirectory( parser.getValue( EXACT ) );
+			Path outputResult = Paths.get( parser.getValue( RESULTS, "results.json" ) );
 
-			if ( parser.hasValue( QUERY ) || parser.hasValue( EXACT ) ) {
-				String flag = parser.hasFlag( QUERY ) ? QUERY : EXACT;
-				Path queryFile = getValidDirectory( parser.getValue( flag ) );
-				Path outputResult = getOutputFile( parser, RESULTS, "results.json" );
-
-				if ( queryFile == null ) {
-					return;
-				}
-
-				if ( parser.hasFlag( QUERY ) ) {
-
-					SearchInvertedIndex.partial( queryFile, outputResult, index );
-				}
-				else if ( parser.hasFlag( EXACT ) ) {
-					SearchInvertedIndex.exact( queryFile, outputResult, index );
-				}
+			if ( queryFile == null ) {
+				return;
 			}
-			
-			/* TODO
-			if (dir flag)
-				build stuff
-			
-			if index flag
-				write index
-			
-			if exact flag
-				search
-			
-			if query flag
-				search
-			
-			if result flag
-				write search
-			*/
+			try {
+				SearchInvertedIndex.exact( queryFile, outputResult, index );
+			}
+			catch ( IOException e ) {
+				System.out.println( "Exact Searching Inverted Index Failed" );
+			}
 		}
-		catch (IOException e ) {
-			// TODO Would be nice to know more about what went wrong? Which file? Was it building or searching or writing?
-			System.out.println( "File may be in use or not exist.." );
-			return;
+		if ( parser.hasValue( QUERY ) ) {
+			Path queryFile = getValidDirectory( parser.getValue( QUERY ) );
+			Path outputResult = Paths.get( parser.getValue( RESULTS, "results.json" ) );
+
+			if ( queryFile == null ) {
+				return;
+			}
+			try {
+				SearchInvertedIndex.partial( queryFile, outputResult, index );
+			}
+			catch ( IOException e ) {
+				System.out.println( "Partial Searching Inverted Index Failed" );
+			}
 		}
+
+		/*
+		 * TODO if (dir flag) build stuff
+		 * 
+		 * if index flag write index
+		 * 
+		 * if exact flag search
+		 * 
+		 * if query flag search
+		 * 
+		 * if result flag write search
+		 */
+
 	}
 
 	/**
@@ -101,7 +112,8 @@ public class Driver {
 		return dir.normalize();
 	}
 
-	// TODO Since this is private, you aren't really getting anything from this one-line method
+	// TODO Since this is private, you aren't really getting anything from this
+	// one-line method
 	/**
 	 * gets the output file from the argument parser or by default uses
 	 * index.json in the current directory
@@ -110,7 +122,9 @@ public class Driver {
 	 * @return the path of the output file
 	 */
 	private static Path getOutputFile( ArgumentParser parser, String flag, String defaulter ) {
-		// TODO If you really want it as a path, add a getPath(String flag, String default)
+
+		// TODO If you really want it as a path, add a getPath(String flag,
+		// String default)
 		return Paths.get( parser.getValue( flag, defaulter ) );
 	}
 }
